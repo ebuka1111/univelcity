ffefe5539066b8ac6ffb3031c1e96a01011df5e8
daniel <marianapat444@gmail.com>
2023-12-05 03:03:32 -0800

    second commit

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from .serializers import UserRegistrationSerializer
+from rest_framework.views import APIView
+
 
 # Create your views here.
+
+class CreateUserView(APIView):
+    def post(self, request, *args, **kwargs):
+        new_data = UserRegistrationSerializer(data=request.data)
+        new_data.is_valid(raise_exception=True)
+        new_data.save()
+        return Response({"Success": "Thanks for signing up"})
